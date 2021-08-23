53f65a7a17f8c93e7d6e357aab7ec3a76fa96897
Amir Pathan <75565798+MDAmirPathan@users.noreply.github.com>
2021-08-23 07:03:49 +0530

    Update settings.py

--- pyshop1/settings.py
+++ pyshop1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tk*+er@n+%vjyd@6riqg*klqid)vw^wy_edvt*_f(^i4u(6+aa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127:01:2', 'fest1.herokuapp.com']
+ALLOWED_HOSTS = ['127:01:2', 'fest1.herokuapp.com', 'https://fest1.herokuapp.com/']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
